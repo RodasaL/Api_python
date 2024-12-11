03611fe4a8930b5a9fdb51f7e844332f28ac667a
Rodrigo Lourenco <95983889+RodasaL@users.noreply.github.com>
2024-12-11 07:31:18 +0000

    Update teste_api.py

--- teste_api.py
+++ teste_api.py
@@ -1,17 +1,10 @@
-# -*- coding: utf-8 -*-
-"""
-API para Classificação de Imagens com TensorFlow Lite
-"""
-
-import os
+import tensorflow as tf
 import numpy as np
 from flask import Flask, request, jsonify
 from PIL import Image
-from tensorflow.lite import Interpreter
 
-# Configurações do TensorFlow Lite
-MODEL_PATH = "animal_classifier_model.tflite"
-NUM_CLASSES = 6
+# Configurações
+MODEL_PATH = "animal_classifier_model.h5"
 TARGET_SIZE = (96, 96)
 
 # Mapear os índices das classes para os seus rótulos reais
@@ -20,41 +13,21 @@ cifar_labels = [
     "cao", "sapo", "cavalo"
 ]
 
-# Otimizações para reduzir logs e uso de GPU
-os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"  # Apenas erros críticos
-os.environ["TF_ENABLE_ONEDNN_OPTS"] = "0"  # Desativar operações oneDNN
-os.environ["CUDA_VISIBLE_DEVICES"] = "-1"  # Forçar uso apenas de CPU
-
 # Inicializar Flask
 app = Flask(__name__)
 
-# Carregar o modelo TensorFlow Lite
+# Carregar o modelo
 try:
-    interpreter = Interpreter(model_path=MODEL_PATH)
-    interpreter.allocate_tensors()
-    print(f"Modelo TensorFlow Lite carregado com sucesso de {MODEL_PATH}")
+    model = tf.keras.models.load_model(MODEL_PATH)
+    print(f"Modelo carregado com sucesso de {MODEL_PATH}")
 except Exception as e:
-    print(f"Erro ao carregar o modelo TensorFlow Lite: {e}")
-    interpreter = None
-
-
-def predict_with_tflite(image):
-    """Faz a previsão usando o modelo TensorFlow Lite."""
-    input_details = interpreter.get_input_details()
-    output_details = interpreter.get_output_details()
-
-    # Configurar entrada
-    interpreter.set_tensor(input_details[0]['index'], image)
-    interpreter.invoke()
-
-    # Obter resultados
-    predictions = interpreter.get_tensor(output_details[0]['index'])
-    return predictions
+    print(f"Erro ao carregar o modelo: {e}")
+    model = None
 
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    if interpreter is None:
+    if model is None:
         return jsonify({"error": "Modelo não carregado"}), 500
 
     if 'file' not in request.files:
@@ -71,7 +44,7 @@ def predict():
         image = image.reshape(1, *TARGET_SIZE, 3)  # Adicionar dimensão do batch
 
         # Fazer a previsão
-        prediction = predict_with_tflite(image)
+        prediction = model.predict(image)
         class_id = np.argmax(prediction)  # Obter o índice da classe com maior probabilidade
         confidence = float(np.max(prediction))  # Confiança da previsão
         class_label = cifar_labels[class_id] if class_id < len(cifar_labels) else "Desconhecida"
